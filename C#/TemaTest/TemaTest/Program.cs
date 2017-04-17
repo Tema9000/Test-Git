539a1ea54406b86373dbde72ab5f39852c9e7ee0
Artem <sherstovao@gmail.com>
2017-04-17 23:06:26 +0300

    Added C# project

---
+++ C#/TemaTest/TemaTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int i = 100;//объявление целочисленной константы i
+            Console.WriteLine(i); //просмотр значения константы
+            
+        }
+    }
+}
