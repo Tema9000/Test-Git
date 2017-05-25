081be6d3b8cde51bd20f1794e6d0143348fc633b
Artem <sherstovao@gmail.com>
2017-05-25 23:29:49 +0300

    Test project was updated

--- C#/TemaTest/TemaTest/Program.cs
+++ C#/TemaTest/TemaTest/Program.cs
@@ -10,8 +10,25 @@ namespace TemaTest
     {
         static void Main(string[] args)
         {
-            const int i = 100;//объявление целочисленной константы i
+            /*const int i = 100;//объявление целочисленной константы i
             Console.WriteLine(i); //просмотр значения константы
+            int a=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("aaa {0}",i);
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= 0; j++) { }
+                Console.Write("*");
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j <= 5; j++) { Console.Write("*"); }
+                Console.WriteLine("*");
+            }
+            }*/
+
+
             
         }
     }
